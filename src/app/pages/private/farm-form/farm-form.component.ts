f37d9640ece4e2455b367bfc078c916588595648
felixmk0 <fmontrakruse@gmail.com>
2025-08-28 21:51:22 +0200

    Nuevo diseño en formulario de creación de granja

--- src/app/pages/private/farm-form/farm-form.component.ts
+++ src/app/pages/private/farm-form/farm-form.component.ts
@@ -1,21 +1,37 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { HlmInput } from '@spartan-ng/helm/input';
 import { HlmButton } from '@spartan-ng/helm/button';
-import { lucideX } from '@ng-icons/lucide';
-import { lucideSave } from '@ng-icons/lucide';
+import { HlmLabel } from '@spartan-ng/helm/label';
+import { lucideCheck, lucideX } from '@ng-icons/lucide';
 import { HlmIcon } from '@spartan-ng/helm/icon';
 import { NgIcon, provideIcons } from '@ng-icons/core';
+import {
+  BrnDialogContent,
+  BrnDialogTrigger,
+  BrnDialogClose,
+} from '@spartan-ng/brain/dialog';
+import {
+  HlmDialog,
+  HlmDialogContent,
+  HlmDialogDescription,
+  HlmDialogFooter,
+  HlmDialogHeader,
+  HlmDialogTitle,
+} from '@spartan-ng/helm/dialog';
 
 export interface Granja {
   id: number;
   nombre: string;
-  ubicacion: string;
-  telefono: string;
+  codigoRega: string;
+  direccion: string;
   email: string;
-  lotes: number;
-  estado: 'Activa' | 'Inactiva';
 }
 
 @Component({
@@ -27,11 +43,21 @@ export interface Granja {
     HlmInput,
     HlmButton,
     NgIcon,
-    HlmIcon
+    HlmIcon,
+    HlmDialog,
+    HlmDialogContent,
+    HlmDialogHeader,
+    HlmDialogTitle,
+    HlmDialogDescription,
+    HlmDialogFooter,
+    BrnDialogTrigger,
+    HlmLabel,
+    BrnDialogClose,
+    BrnDialogContent,
   ],
-  providers: [provideIcons({ lucideX, lucideSave })],
+  providers: [provideIcons({ lucideCheck, lucideX })],
   templateUrl: './farm-form.component.html',
-  styleUrls: ['./farm-form.component.css']
+  styleUrls: ['./farm-form.component.css'],
 })
 export class FarmFormComponent {
   @Input() granja?: Granja;
@@ -44,21 +70,22 @@ export class FarmFormComponent {
 
   ngOnInit() {
     this.form = this.fb.group({
-      nombre: this.fb.nonNullable.control(this.granja?.nombre || '', Validators.required),
-      ubicacion: this.fb.nonNullable.control(this.granja?.ubicacion || '', Validators.required),
-      telefono: this.fb.nonNullable.control(this.granja?.telefono || '', [
-        Validators.required,
-        Validators.pattern(/^\+?\d{9,15}$/)
-      ]),
+      nombre: this.fb.nonNullable.control(
+        this.granja?.nombre || '',
+        Validators.required
+      ),
+      'codigo-rega': this.fb.nonNullable.control(
+        this.granja?.codigoRega || '',
+        Validators.required
+      ),
+      direccion: this.fb.nonNullable.control(
+        this.granja?.direccion || '',
+        Validators.required
+      ),
       email: this.fb.nonNullable.control(this.granja?.email || '', [
         Validators.required,
-        Validators.email
+        Validators.email,
       ]),
-      lotes: this.fb.nonNullable.control(this.granja?.lotes || 0, [
-        Validators.required,
-        Validators.min(1)
-      ]),
-      estado: this.fb.nonNullable.control<'Activa' | 'Inactiva'>(this.granja?.estado || 'Activa', Validators.required)
     });
   }
 
@@ -66,16 +93,6 @@ export class FarmFormComponent {
     if (this.form.valid) {
       const nuevaGranja: Granja = { ...this.granja, ...this.form.value };
       this.guardar.emit(nuevaGranja);
-      this.form.reset({ estado: 'Activa', lotes: 0 });
     }
-  }
-
-  toggleEstado() {
-  const current = this.form.get('estado')?.value;
-  this.form.get('estado')?.setValue(current === 'Activa' ? 'Inactiva' : 'Activa');
-}
-
-  cancelar() {
-    this.cancelarForm.emit();
   }
 }
